5b3bb9ddcc6aeebd6cde282c38bd40a6f7aa854a
Mithrandir <leniwuihobbit@gmail.com>
2023-09-24 18:11:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,8 @@ def main(_, msg: Message):
     count_kicks = 0
     #for member in app.get_chat_members(chat.id, filter!=enums.ChatMembersFilter.ADMINISTRATORS): #chat.iter_members(): 
     #for member in app.get_chat_members(chat.id, filter!=enums.ChatMembersFilter.BOTS): #chat.iter_members(): 
-    for member in app.get_chat_members(chat.id, filter!=enums.ChatMembersFilter.ADMINISTRATORS): #chat.iter_members(): 
+    #for member in app.get_chat_members(chat.id, filter!=enums.ChatMembersFilter.ADMINISTRATORS): #chat.iter_members(): 
+    for member in app.get_chat_members(chat.id): #chat.iter_members(): 
         #if not member.can_manage_chat:
         #chat.kick_member(member.user.id)
         try:
