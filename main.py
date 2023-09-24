46c9e0f08f64efc8cf1f387cefaa1db7c9553bb1
Mithrandir <leniwuihobbit@gmail.com>
2023-09-24 17:52:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def main(_, msg: Message):
         try:
             print(member.user.id)
             #print(member.user.name)   
-            if (member.user.id != 6656443250) AND (member.user.id != 5224257270):
+            if ((member.user.id != 6656443250) and (member.user.id != 5224257270)):
                 
                 print(f'deleting {member.user.id}')
                 app.ban_chat_member(chat.id, (member.user.id))
