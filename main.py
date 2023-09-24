616c7cf35b4fbe53fb3ed67fac60f8613db3f5c7
Mithrandir <leniwuihobbit@gmail.com>
2023-09-24 16:41:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,18 +32,18 @@ def main(_, msg: Message):
     chat = msg.chat
     me = chat.get_member(app.get_me().id)
     #if chat.get_member(msg.from_user.id).can_manage_chat and me.can_restrict_members and me.can_delete_messages:
-        try:
-            msg.reply(STARTED.format(chat.members_count))
-            count_kicks = 0
-            for member in chat.iter_members():
-                if not member.can_manage_chat:
-                    chat.kick_member(member.user.id)
-                    count_kicks += 1
-            msg.reply(FINISH.format(count_kicks))
-        except Exception as e:
-            msg.reply(ERROR.format(str(e)))
-    else:
-        msg.reply(ADMIN_NEEDED)
+    try:
+        msg.reply(STARTED.format(chat.members_count))
+        count_kicks = 0
+        for member in chat.iter_members():
+            if not member.can_manage_chat:
+                chat.kick_member(member.user.id)
+                count_kicks += 1
+        msg.reply(FINISH.format(count_kicks))
+    except Exception as e:
+        msg.reply(ERROR.format(str(e)))
+    #else:
+        #msg.reply(ADMIN_NEEDED)
 
 
 @app.on_message(filters.group & filters.service, group=2)
